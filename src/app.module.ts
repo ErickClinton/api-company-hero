e549c4aafec7ed2e9065c875c48444b24143e989
Erick Clinton <erickleite2009321@gmail.com>
2024-05-24 14:02:07 -0300

    feat: adding recomendation module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { RecomendationModule } from "./api/app/recomendation/recomendation.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [RecomendationModule],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
