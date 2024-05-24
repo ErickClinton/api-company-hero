90653ab2a4adcfb52ec69081347f283ce387decb
Erick Clinton <erickleite2009321@gmail.com>
2024-05-24 17:35:48 -0300

    feat: adding authentication module

--- src/api/app/recomendation/producer/spotify/Spotify.module.ts
+++ src/api/app/recomendation/producer/spotify/Spotify.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { HttpModule } from "@nestjs/axios";
 import { SpotifyService } from "./Spotify.service";
+import { AuthenticationModule } from "./authentication/authentication.module";
 
 @Module({
-    imports: [HttpModule],
+    imports: [HttpModule, AuthenticationModule],
     controllers: [],
     providers: [SpotifyService],
     exports: [SpotifyService]
