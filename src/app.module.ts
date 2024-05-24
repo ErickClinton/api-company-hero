d89859fda1e4d0079aaebf629013611f94f46ff0
Erick Clinton <erickleite2009321@gmail.com>
2024-05-24 14:27:31 -0300

    feat: add openWeather module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { RecomendationModule } from "./api/app/recomendation/recomendation.module";
+import { OpenWeatherModule } from "./api/app/recomendation/producer/open-weather/openWeather.module";
 
 @Module({
-    imports: [RecomendationModule],
+    imports: [RecomendationModule, OpenWeatherModule],
     controllers: [AppController],
     providers: [AppService]
 })
