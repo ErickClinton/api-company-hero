8e85572ec236f36a898f2fe63ade7a9eb1b66b5d
Erick Clinton <erickleite2009321@gmail.com>
2024-05-24 14:29:04 -0300

    feat: implements service openWeather

--- src/api/app/recomendation/recomendation.service.ts
+++ src/api/app/recomendation/recomendation.service.ts
@@ -1,16 +1,18 @@
 import { Injectable, Logger } from "@nestjs/common";
+import { OpenWeatherService } from "./producer/open-weather/openWeather.service";
 
 @Injectable()
 export class RecomendationService {
     private readonly logger = new Logger(RecomendationService.name);
 
+    constructor(private readonly openWeatherService: OpenWeatherService) {}
     getRecomendation(city: string): string {
         try {
             this.logger.log(`Start service getRecomendation - Request - ${JSON.stringify({ city })}`);
+            const response = this.openWeatherService.getTemperatureByCity(city);
+            this.logger.log(`End service getRecomendation - Request - ${JSON.stringify({ response })}`);
 
-            this.logger.log(`End service getRecomendation - Request - ${JSON.stringify({ city })}`);
-
-            return city;
+            return response;
         } catch (error) {
             this.logger.error(`Error service getRecomendation - Error - ${JSON.stringify({ error })}`);
             throw new Error(error);
