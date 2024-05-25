5d97b20ca1fb3672426ac5322d25cdeae0edf3b5
Erick Clinton <erickleite2009321@gmail.com>
2024-05-25 00:51:35 -0300

    feat: return of necessary data

--- src/api/app/recomendation/recomendation.module.ts
+++ src/api/app/recomendation/recomendation.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
 import { RecomendationController } from "./recomendation.controller";
 import { RecomendationService } from "./recomendation.service";
-import { OpenWeatherModule } from "./producer/open-weather/openWeather.module";
-import { SpotifyModule } from "./producer/spotify/Spotify.module";
+import { SpotifyModule } from "../producer/spotify/spotify.module";
+import { OpenWeatherModule } from "../producer/open-weather/openWeather.module";
 
 @Module({
     imports: [OpenWeatherModule, SpotifyModule],
